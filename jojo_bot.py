b732f94e6c9b164ac6ffdc09a0b38da7b894bce5
lyangas <44424726+lyangas@users.noreply.github.com>
2020-10-09 14:05:08 +0700

    Update jojo_bot.py
    исправлены ошибки орфографические и в кодировке символов в 21-й строке

--- jojo_bot.py
+++ jojo_bot.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 replicas = [
     'ты проходишь возле рынка.\nпройти мимо?',
-    'ты проходишь мимо, но тут тебя подозвал к себе какой-то неперсточник-циган. \nсыграть с ним?',
-    'опаа, а тут на самом входе сидит некоторый тип циганской наружности и играет в наперстки! \nсыграть с ним?',
+    'ты проходишь мимо, но тут тебя подозвал к себе какой-то неперсточник-цыган. \nсыграть с ним?',
+    'опаа, а тут на самом входе сидит некоторый тип цыганской наружности и играет в наперстки! \nсыграть с ним?',
     'он сказал что у тебя плохая прическа. но ты гребаный Жоский Жоске и у тебя просто не осталось выбора после такой дерзости!',
     'он кладет шарик под 3-й стаканчик. затем меняет местами 2 и 1. \nшарик в стаканчике номер 3?',
     'ай маладца! какой красавчик у нас играит!\n\nты поставил на кон мамку.\n\nтеперь он кладет шарик под 1-й стаканчик. затем меняет местами 1 и 2. \nшарик в стаканчике номер 1?',
@@ -18,7 +18,7 @@ replicas = [
     'я призываю свой стояк! Коцаный Янтарь - удар кулаком!!! \n\nнаперсточник говорит:\nУХ, мать, как большо ты шлепнул мне по щеке своим стояком! ты опасный поц!\n\nи тут ты заметил, что рядом валяется бита - схватить ее для удара? из альтернатив - ты можешь рассказать шарлотану о его мамаше...',
     'ты хорошенько погладил его битой по голове\n\nКАК ТЫ ЭТО ДЕЛАЕШЬ???!! прекрати немедленно! - кричит шуллер\n\nты всего на секунду отвлекся на аквариум с черепахами в соседнем прилавке, как вдруг - машенник пропал!\n\nобернуться назад?',
     '...но и тут он оказывается быстрее тебя и мнгновенно хватает эту биту и бъет тебя в поддых!\n\nты всего на секунду отвлекся на аквариум с черепахами в соседнем прилавке, как вдруг - машенник пропал!\n\nобернуться назад?',
-    'твоя мамка-цыганка! - крикнул ты еэтому типу и его лицо залилось красным цветом! переполняемый эмоциями, он кричит тебе: \n\nвообще-то, это расизм, больной ты ублюдок! и как ты посмел такое сказать про эту великолепную женщину, которая с детства дает мне столько заботы, добра и побоев! она взрастила из меня законопослушного гражданина, который встает каждый день с одной мысью: надо нести людям счастье... не по 1000р за грамм, а за 900р! а ну забери слова обратно!\n\nты всего на секунду отвлекся на аквариум с черепахами в соседнем прилавке, как вдруг - машенник пропал!\n\nобернуться назад?',
+    'твоя мамка-цыганка! - крикнул ты этому типу и его лицо залилось красным цветом! переполняемый эмоциями, он кричит тебе: \n\nвообще-то, это расизм, больной ты ублюдок! и как ты посмел такое сказать про эту великолепную женщину, которая с детства дает мне столько заботы, добра и побоев! она взрастила из меня законопослушного гражданина, который встает каждый день с одной мысью: надо нести людям счастье... не по 1000р за грамм, а за 900р! а ну забери слова обратно!\n\nты всего на секунду отвлекся на аквариум с черепахами в соседнем прилавке, как вдруг - машенник пропал!\n\nобернуться назад?',
     'ты обернулся и видишь как он берет биту из отдела "все для домашнего уюта", и со всей силы бъешь его  МУДА-МУДА-МУДА!\nон лежит на асфальте перед тобой, напуганный...\n\nспросишь его о силе его стояка?',
     'ты обернулся и тут же получил удар по голове! откуда он взялся? - эта мысль уже не проскочит в твоей голове, так как ты лежишь без сознания. THE END',
     'ты видишь, как он выскакивает из-за соседнего прилавка и летит не тебя с испуганными глазами! ты не растерялся: бъешь его что есть мочи - теперь он лежит на асфальте.\n\nспросишь его о силе его стояка?',
@@ -99,90 +99,71 @@ my_timer = MyTimer()
 attempt_count = 0
 index_of_ip = 0
 
-tariff_broadband_df = pd.read_csv('socks5.csv', names=['ip', 'country'])
-my_proxies = list(tariff_broadband_df.ip)
 
-while True:
+bot = telebot.TeleBot('1215716564:AAEhpwiQgSMX912DUmPBrutF-o2DaywpG0U')
+
+dialogues = {}
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('Да', 'Нет', 'Начать с начала')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
     try:
-        my_proxy = my_proxies[index_of_ip]
-        apihelper.proxy = {'https':'socks5h://' + my_proxy}
-        bot = telebot.TeleBot('1215716564:AAEhpwiQgSMX912DUmPBrutF-o2DaywpG0U')
+        dialogues[message.chat.id]
+    except Exception:
+        dialogues[message.chat.id] = {}
+        pass
+    dialogues[message.chat.id]['index'] = 0
+    dialogues[message.chat.id]['time'] = time.time()
+    dialogues[message.chat.id]['score'] = 0
+    bot.send_message(message.chat.id, 'ну, начнем :)', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'ты проходишь возле рынка.\nпройти мимо?', reply_markup=keyboard1)
 
-        dialogues = {}
-
-        keyboard1 = telebot.types.ReplyKeyboardMarkup()
-        keyboard1.row('Да', 'Нет', 'Начать с начала')
-
-        @bot.message_handler(commands=['start'])
-        def start_message(message):
-            try:
-                dialogues[message.chat.id]
-            except Exception:
-                dialogues[message.chat.id] = {}
-                pass
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    try:
+        # если решил начать чат с начала по своей воле
+        if 'начать с начала' in message.text.lower():
+            bot.send_message(message.chat.id, 'ты проходишь возле рынка.\nпройти мимо?', reply_markup=keyboard1)
+            dialogues[message.chat.id] = {}
             dialogues[message.chat.id]['index'] = 0
             dialogues[message.chat.id]['time'] = time.time()
             dialogues[message.chat.id]['score'] = 0
-            bot.send_message(message.chat.id, 'ну, начнем :)', reply_markup=keyboard1)
-            bot.send_message(message.chat.id, 'ты проходишь возле рынка.\nпройти мимо?', reply_markup=keyboard1)
+            return
 
-        @bot.message_handler(content_types=['text'])
-        def send_text(message):
-            try:
-                # если решил начать чат с начала по своей воле
-                if 'начать с начала' in message.text.lower():
-                    bot.send_message(message.chat.id, 'ты проходишь возле рынка.\nпройти мимо?', reply_markup=keyboard1)
-                    dialogues[message.chat.id] = {}
-                    dialogues[message.chat.id]['index'] = 0
+        # если оказался в начале истроии так как закончил ее
+        if dialogues[message.chat.id]['index'] == 0:
+            dialogues[message.chat.id]['score'] = 0
+
+        if 'да' in message.text.lower():
+            if dialogues[message.chat.id]['index'] in [5, 7, 11, 12, 15, 17]:
+                dialogues[message.chat.id]['score'] += 1
+
+            if dialogues[message.chat.id]['index'] in [15, 17]:
+                if dialogues[message.chat.id]['score'] < 4:
                     dialogues[message.chat.id]['time'] = time.time()
-                    dialogues[message.chat.id]['score'] = 0
-                    return
+            
+            choice = 'да'
+        if 'нет' in message.text.lower():
+            choice = 'нет'
+        if choice in ['да','нет']:
+            index = dialogues[message.chat.id]['index']
+            dialogue = dialogues[message.chat.id]
+            dialogues[message.chat.id]['index'] = get_next_replic(index, choice, dialogue)
+            
+            answer = replicas[dialogues[message.chat.id]['index']]
+            bot.send_message(message.chat.id, answer)
+        '''for answer_msg in answer.split('\n\n'):
+            bot.send_message(message.chat.id, answer_msg)'''
+    except KeyError:
+        dialogues[message.chat.id] = {}
+        dialogues[message.chat.id]['index'] = 0
+        dialogues[message.chat.id]['time'] = time.time()
+        dialogues[message.chat.id]['score'] = 0
 
-                # если оказался в начале истроии так как закончил ее
-                if dialogues[message.chat.id]['index'] == 0:
-                    dialogues[message.chat.id]['score'] = 0
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
 
-                if 'да' in message.text.lower():
-                    if dialogues[message.chat.id]['index'] in [5, 7, 11, 12, 15, 17]:
-                        dialogues[message.chat.id]['score'] += 1
-
-                    if dialogues[message.chat.id]['index'] in [15, 17]:
-                        if dialogues[message.chat.id]['score'] < 4:
-                            dialogues[message.chat.id]['time'] = time.time()
-                    
-                    choice = 'да'
-                if 'нет' in message.text.lower():
-                    choice = 'нет'
-                if choice in ['да','нет']:
-                    index = dialogues[message.chat.id]['index']
-                    dialogue = dialogues[message.chat.id]
-                    dialogues[message.chat.id]['index'] = get_next_replic(index, choice, dialogue)
-                    
-                    answer = replicas[dialogues[message.chat.id]['index']]
-                    bot.send_message(message.chat.id, answer)
-                '''for answer_msg in answer.split('\n\n'):
-                    bot.send_message(message.chat.id, answer_msg)'''
-            except KeyError:
-                dialogues[message.chat.id] = {}
-                dialogues[message.chat.id]['index'] = 0
-                dialogues[message.chat.id]['time'] = time.time()
-                dialogues[message.chat.id]['score'] = 0
-
-        @bot.message_handler(content_types=['sticker'])
-        def sticker_id(message):
-            print(message)
-
-        print('current ip = {}'.format(my_proxy))
-        bot.polling(none_stop=True)
-
-    except Exception as e:
-        print(str(e))
-        attempt_count += 1
-        if attempt_count > 5:
-            attempt_count = 0
-            index_of_ip += 1
-            if index_of_ip >= len(my_proxies):
-                index_of_ip = 0
-        time.sleep(1)
-        print ("Internet error! ip_index={} attempt_count={}".format(index_of_ip, attempt_count))
-
+bot.polling(none_stop=True)
